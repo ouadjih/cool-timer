eddf6dd5feeef933183b1b57dc93e21cdb45a7f5
ouadjih <47463757+ouadjih@users.noreply.github.com>
2023-01-05 15:03:31 +0100

    added second example

--- main.cpp
+++ main.cpp
@@ -1,4 +1,42 @@
 #include"timer.h"
+#include<iomanip>
+#include<iostream>
+using namespace std;
 int main(){
+    int a =0;
+    cout<< "hello timer";
+    Timer t;
+    /*first example
+    cout<<"Start timer :"<<endl;
+    t.Restart();
+    cout<<"Tick1 : "<<fixed<<setprecision(16)<<setfill('0')<< t.GetDeltaTime()<<endl;
+    cout<< "..."<<endl;
+    cout<<"Tick2 : "<<fixed<<setprecision(16)<<setfill('0')<<t.GetDeltaTime()<<endl;
+    cout<< "..."<<endl;
+    cout<< "..."<<endl;
+    cout<<"Tick3 : "<<fixed<<setprecision(16)<<setfill('0')<<t.GetDeltaTime()<<endl;
+
+    cout<<"amount of time since start : "<<t.GetElapsedTime()<<endl;
+    /*while(t.GetDeltaTime()< 2.f){
+        cout<<fixed<<setprecision(8)<<setfill('0')<< t.GetDeltaTime()<<endl;
+        a++;
+    }
+    cout<<"a= "<<a;*/
+
+    /* second example*/
+    while(t.GetElapsedTime()< 1.f){
+         cout<<fixed<<setprecision(7)<<setfill('0')<< t.GetElapsedTime()<<endl;
+    }
+    float timeout = 3.f;
+    bool running = true;
+    t.Restart();
+    while(running){
+        int elapsed_counts = 0;
+        if(t.GetElapsedTime() > timeout){
+            cout<< "3 seconds elapsed";
+            t.Restart();
+            elapsed_counts++;
+        }
+    }
 
 return 0;}
--- timer.h
+++ timer.h
@@ -4,22 +4,26 @@ class Timer{
     public:
         Timer(){
             _start_time = _current_time = _last_time = _last_delta_tick = std::chrono::system_clock::now();
-            _delta_time = std::chrono::duration<float>(0);
+            _delta_time = std::chrono::duration<double>(0);
         };
-        void Reset(){
+        //Reset the start_time to now
+        void Restart(){
             _start_time = std::chrono::system_clock::now();
         };
+        // returns the duration between last tick(last call of delta time) and current time.
         float GetDeltaTime(){
             _current_time = std::chrono::system_clock::now();
-             _delta_time = _current_time - _last_delta_tick;
+            _delta_time = _current_time - _last_delta_tick;
+            _last_delta_tick = _current_time;
              return _delta_time.count();
         }
-        float ElapsedTime(){
+        // returns the duration between the start time and current time
+        float GetElapsedTime(){
              _current_time = std::chrono::system_clock::now();
-             std::chrono::duration<float> _elapsed_time = _start_time - _current_time;
+             std::chrono::duration<double> _elapsed_time = _current_time - _start_time;
              return _elapsed_time.count();
         }
     private:
         std::chrono::system_clock::time_point _start_time, _current_time,_last_time, _last_delta_tick;
-        std::chrono::duration<float> _delta_time;
+        std::chrono::duration<double> _delta_time;
 };
