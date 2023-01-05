2d93faa588968acd7fdceefd9fb139dd8962ee2d
ouadjih <47463757+ouadjih@users.noreply.github.com>
2023-01-05 11:18:12 +0100

    added full timer class

---
+++ main.cpp
@@ -0,0 +1,4 @@
+#include"timer.h"
+int main(){
+
+return 0;}
--- timer.h
+++ timer.h
@@ -3,14 +3,22 @@
 class Timer{
     public:
         Timer(){
-            _start_time=
-            _current_time=
-            _last_time=
-            _last_delta_tick=
+            _start_time = _current_time = _last_time = _last_delta_tick = std::chrono::system_clock::now();
+            _delta_time = std::chrono::duration<float>(0);
         };
-        void Reset();
-        float GetDeltaTime();
-        float ElapsedTime();
+        void Reset(){
+            _start_time = std::chrono::system_clock::now();
+        };
+        float GetDeltaTime(){
+            _current_time = std::chrono::system_clock::now();
+             _delta_time = _current_time - _last_delta_tick;
+             return _delta_time.count();
+        }
+        float ElapsedTime(){
+             _current_time = std::chrono::system_clock::now();
+             std::chrono::duration<float> _elapsed_time = _start_time - _current_time;
+             return _elapsed_time.count();
+        }
     private:
         std::chrono::system_clock::time_point _start_time, _current_time,_last_time, _last_delta_tick;
         std::chrono::duration<float> _delta_time;
